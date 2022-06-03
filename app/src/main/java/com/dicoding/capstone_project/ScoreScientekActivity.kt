a2d88488e0fe1115616f39fc50310769c81b1bc6
JohannesKase <86041833+JohannesKase@users.noreply.github.com>
2022-06-03 23:43:03 +0700

    Update UI
    - Add both input score UI for science tech and social humanity
    - Update on main UI

---
+++ app/src/main/java/com/dicoding/capstone_project/ScoreScientekActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.capstone_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScoreScientekActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scorescientek)
+    }
+}
---
+++ app/src/main/java/com/dicoding/capstone_project/ScoreSoshumActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.capstone_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScoreSoshumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score_soshum)
+    }
+}
